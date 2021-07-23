fdb26616bf71010f4bfdaede2420e993093ee755
Eduardo Kose <eduardo.kose@kptl.com.br>
2021-07-22 23:55:27 -0300

    Fix

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :hello, Hello.Repo,
   username: "postgres",
   password: "postgres",
   database: "hello_dev",
-  hostname: "localhost",
+  hostname: "database-test.cszaegpah1te.us-east-1.rds.amazonaws.com",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
